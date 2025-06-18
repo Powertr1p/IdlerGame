598c1d4e5e4c30c20c598f613ef6dbf4a211a267
Anton Maevsky <mae-anton@mail.ru>
2025-06-18 22:37:37 +0300

    PlayerMovement

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(BoxCollider))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private FixedJoystick _fixedJoystick;
+    [SerializeField] private float _moveSpeed;
+
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        _rigidbody.linearVelocity = new Vector3(_fixedJoystick.Horizontal * _moveSpeed, _rigidbody.linearVelocity.y,
+            _fixedJoystick.Vertical * _moveSpeed);
+
+        if (_fixedJoystick.Horizontal != 0 || _fixedJoystick.Vertical != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(_rigidbody.linearVelocity);
+        }
+    }
+}

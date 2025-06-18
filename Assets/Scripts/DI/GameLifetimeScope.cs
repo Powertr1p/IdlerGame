0a92ba4483486ee5a9c09fcfd310a8fe8e3c3226
Powertr1p <sc2aru@gmail.com>
2025-06-18 14:49:15 +0400

    added DI framework and setting it

---
+++ Assets/Scripts/DI/GameLifetimeScope.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+public class GameLifetimeScope : LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+       //todo: register services
+    }
+}

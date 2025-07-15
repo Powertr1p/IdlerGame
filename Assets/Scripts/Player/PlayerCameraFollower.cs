b0e0208bbbff269f66e2823e0a55ca077053ec28
Anton Maevsky <mae-anton@mail.ru>
2025-07-15 19:14:05 +0300

    more PlayerCameraFollower.cs tuning

--- Assets/Scripts/Player/PlayerCameraFollower.cs
+++ Assets/Scripts/Player/PlayerCameraFollower.cs
@@ -1,26 +1,18 @@
 using UnityEngine;
 
-public class PlayerFollowCamera : MonoBehaviour
+public class PlayerCameraFollower : MonoBehaviour
 {
     [Header("Player")] 
     [SerializeField] private Transform _target;
 
     [Header("Smoothing")] 
-    [SerializeField, Range(0f, 25f)] private float _smoothSpeed;
+    [SerializeField, Range(0f, 15f)] private float _smoothSpeed;
     
     [Header("Camera Offset")] 
     [SerializeField, Range(1f, 25f)] private float _offsetY;
     [SerializeField, Range(-25f, 25f)] private float _offsetZ;
-
-    private float _offsetX;
     
-    private Vector3 _offset => new (_offsetX, _offsetY, _offsetZ);
-    private Vector3 _initialPosition;
-
-    private void Awake()
-    {
-        _initialPosition = transform.position;
-    }
+    private Vector3 _offset => new (0, _offsetY, _offsetZ);
 
     private void LateUpdate()
     {
@@ -29,13 +21,18 @@ public class PlayerFollowCamera : MonoBehaviour
 
     private void OnValidate()
     {
-        transform.position = _initialPosition + new Vector3(0, _offsetY, _offsetZ);
+        transform.position = CalculateDesiredPosition();
     }
 
     private void FollowTarget()
     {
-        Vector3 desiredPosition = _target.position + _offset;
+        Vector3 desiredPosition = CalculateDesiredPosition();
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
+    }
+    
+    private Vector3 CalculateDesiredPosition()
+    {
+        return _target.position + _offset;
     }
 }

5b793ea57daf53722249a5c0f24a934a7a85e19e
Powertr1p <sc2aru@gmail.com>
2025-07-17 16:01:17 +0400

    added temp logic to change tools

---
+++ Assets/Scripts/Inventory/EquipmentItems/EquipmentRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Scriptable;
+using UnityEngine;
+
+namespace Inventory.EquipmentItems
+{
+    public class EquipmentRepository : MonoBehaviour
+    {
+        [SerializeField] private List<ToolData> _toolData;
+
+        public ToolData GetEquipment(int id)
+        {
+            for (int i = 0; i < _toolData.Count; i++)
+            {
+                if (_toolData[i].ItemId == id)
+                {
+                    return _toolData[i];
+                }
+            }
+            
+            return null;
+        }
+    }
+}

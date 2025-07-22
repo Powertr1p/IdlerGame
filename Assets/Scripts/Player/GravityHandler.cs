9c5f3bd9076f2954123a16e01bdd9eb7e995bd3a
Anton Maevsky <mae-anton@mail.ru>
2025-07-22 19:39:35 +0300

    added gravity for player

---
+++ Assets/Scripts/Player/GravityHandler.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class GravityHandler : MonoBehaviour
+{
+    [SerializeField] private float _gravityForce;
+    
+    private CharacterController _characterController;
+    private Vector3 _velocityVector;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        ApplyGravity();
+    }
+
+    private void ApplyGravity()
+    {
+        if (!_characterController.isGrounded)
+        {
+            _velocityVector.y -= _gravityForce * Time.deltaTime;
+        }
+        else
+        {
+            _velocityVector.y = -0.5f;
+        }
+
+        _characterController.Move(_velocityVector * Time.deltaTime);
+    }
+}

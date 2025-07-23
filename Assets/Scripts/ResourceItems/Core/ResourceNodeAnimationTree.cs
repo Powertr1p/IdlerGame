0c11abae59448c0b33b5d09ecac72482aa090c3b
Anton Maevsky <mae-anton@mail.ru>
2025-07-23 23:09:33 +0300

    test

--- Assets/Scripts/ResourceItems/Core/ResourceNodeAnimationTree.cs
+++ Assets/Scripts/ResourceItems/Core/ResourceNodeAnimationTree.cs
@@ -8,7 +8,6 @@ namespace GameItems.Core
         public override void AnimateOnHit()
         {
             transform.DOPunchPosition(Vector3.forward * _pulsePower, _pulseDuration);
-            //test
         }
     }
 }

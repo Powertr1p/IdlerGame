d3eeee82e16af9c774ad56f58526f32e8fa0f284
Anton Maevsky <mae-anton@mail.ru>
2025-08-19 19:30:28 +0300

    added tests animation choose in unity

--- Assets/Scripts/Player/PlayerCameraFollower.cs
+++ Assets/Scripts/Player/PlayerCameraFollower.cs
@@ -11,8 +11,14 @@ public class PlayerCameraFollower : MonoBehaviour
     [Header("Camera Offset")] 
     [SerializeField, Range(1f, 25f)] private float _offsetY;
     [SerializeField, Range(-25f, 25f)] private float _offsetZ;
-    
-    private Vector3 _offset => new (0, _offsetY, _offsetZ);
+
+    private Vector3 _offset => new(0, _offsetY, _offsetZ);
+    private Transform _cachedTransform;
+
+    private void Awake()
+    {
+        _cachedTransform = transform;
+    }
 
     private void LateUpdate()
     {
@@ -27,11 +33,11 @@ public class PlayerCameraFollower : MonoBehaviour
     private void FollowTarget()
     {
         Vector3 desiredPosition = CalculateDesiredPosition();
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed * Time.deltaTime);
-        //todo: в апдейте получается постоянно GetComponent<Transform>();
-        transform.position = smoothedPosition;
+        Vector3 smoothedPosition =
+            Vector3.Lerp(_cachedTransform.position, desiredPosition, _smoothSpeed * Time.deltaTime);
+        _cachedTransform.position = smoothedPosition;
     }
-    
+
     private Vector3 CalculateDesiredPosition()
     {
         return _target.position + _offset;
--- Assets/Scripts/ResourceItems/Core/ResourceNodeAnimationTree.cs
+++ Assets/Scripts/ResourceItems/Core/ResourceNodeAnimationTree.cs
@@ -1,3 +1,4 @@
+using System;
 using DG.Tweening;
 using UnityEngine;
 
@@ -5,27 +6,47 @@ namespace GameItems.Core
 {
     public class ResourceNodeAnimationTree : ResourceNodeAnimationBase
     {
+        //енам временный, для выбора более подходящей анимации, под каждый надо настраивать power и duration
+        private enum AnimationType
+        {
+            RotateAndBack,
+            Punch,
+            EndlessRotate
+        }
+
+        [SerializeField] private AnimationType _animationType;
+
         public override void AnimateOnHit()
         {
             KillSequence();
-            
-            //на выбор один из тестов
-            DoRotateAndBackTest();
-            //DoPunchTest();
-            //DoEndlessRotateTest();
+
+            switch (_animationType)
+            {
+                case AnimationType.RotateAndBack:
+                    DoRotateAndBackTest();
+                    break;
+                case AnimationType.Punch:
+                    DoPunchTest();
+                    break;
+                case AnimationType.EndlessRotate:
+                    DoEndlessRotateTest();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
-        
+
         private void DoRotateAndBackTest()
         {
             Quaternion originalRotation = transform.rotation;
-            
+
             Sequence = DOTween.Sequence()
                 .Append(transform.DORotate(new Vector3(0, 30, 0), AnimationDuration, RotateMode.LocalAxisAdd))
                 .Append(transform.DORotate(originalRotation.eulerAngles, AnimationDuration))
                 .SetEase(Ease.OutBack)
                 .OnStart(PlayParticles);
         }
-        
+
         private void DoPunchTest()
         {
             Sequence = DOTween.Sequence()

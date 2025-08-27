37000a5d34c7f61df83bcdc041698cb578f4a218
Anton Maevsky <mae-anton@mail.ru>
2025-08-27 18:54:31 +0300

    ResourceNodeAnimationTree fin

--- Assets/Scripts/ResourceItems/Core/ResourceNodeAnimationTree.cs
+++ Assets/Scripts/ResourceItems/Core/ResourceNodeAnimationTree.cs
@@ -1,4 +1,3 @@
-using System;
 using DG.Tweening;
 using UnityEngine;
 
@@ -6,59 +5,19 @@ namespace GameItems.Core
 {
     public class ResourceNodeAnimationTree : ResourceNodeAnimationBase
     {
-        //енам временный, для выбора более подходящей анимации, под каждый надо настраивать power и duration
-        private enum AnimationType
-        {
-            RotateAndBack,
-            Punch,
-            EndlessRotate
-        }
-
-        [SerializeField] private AnimationType _animationType;
-
         public override void AnimateOnHit()
         {
             KillSequence();
-
-            switch (_animationType)
-            {
-                case AnimationType.RotateAndBack:
-                    DoRotateAndBackTest();
-                    break;
-                case AnimationType.Punch:
-                    DoPunchTest();
-                    break;
-                case AnimationType.EndlessRotate:
-                    DoEndlessRotateTest();
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
+            DoRotate();
         }
 
-        private void DoRotateAndBackTest()
+        private void DoRotate()
         {
             Quaternion originalRotation = transform.rotation;
 
             Sequence = DOTween.Sequence()
                 .Append(transform.DORotate(new Vector3(0, 30, 0), AnimationDuration, RotateMode.LocalAxisAdd))
                 .Append(transform.DORotate(originalRotation.eulerAngles, AnimationDuration))
-                .SetEase(Ease.OutBack)
-                .OnStart(PlayParticles);
-        }
-
-        private void DoPunchTest()
-        {
-            Sequence = DOTween.Sequence()
-                .Append(transform.DOPunchPosition(Vector3.up * AnimationPower, AnimationDuration))
-                .SetEase(Ease.OutBounce)
-                .OnStart(PlayParticles);
-        }
-
-        private void DoEndlessRotateTest()
-        {
-            Sequence = DOTween.Sequence()
-                .Append(transform.DORotate(new Vector3(0, 30, 0), AnimationDuration, RotateMode.LocalAxisAdd))
                 .SetEase(Ease.OutBack)
                 .OnStart(PlayParticles);
         }
